f519e583e7f0db3dcf8fe3a34de9fdd56068030e
Felix Hanley <felix@userspace.com.au>
2017-11-23 14:42:55 +1100

    Update JSON formatter

--- json_writer.go
+++ json_writer.go
@@ -1,14 +1,28 @@
 package logger
 
 import (
+	"bufio"
 	"encoding/json"
-	"io"
 )
 
 type JSONWriter struct{}
 
-func (jw JSONWriter) Write(w io.Writer, m Message) {
-	err := json.NewEncoder(w).Encode(m)
+func NewJSONWriter() *JSONWriter {
+	return &JSONWriter{}
+}
+
+func (jw JSONWriter) Write(w *bufio.Writer, m Message) {
+	vals := map[string]interface{}{
+		"@name":  m.Name,
+		"@level": m.Level.String(),
+		"@time":  m.Time,
+	}
+
+	for i := 0; i < len(m.Fields); i = i + 2 {
+		vals[toString(m.Fields[i])] = m.Fields[i+1]
+	}
+
+	err := json.NewEncoder(w).Encode(vals)
 	if err != nil {
 		panic(err)
 	}
