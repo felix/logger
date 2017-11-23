75a2066bbfcd047c622d5defd1794804ce747366
Felix Hanley <felix@userspace.com.au>
2017-11-23 14:43:50 +1100

    Add JSON tests

--- logger_test.go
+++ logger_test.go
@@ -2,12 +2,14 @@ package logger
 
 import (
 	"bytes"
-	"fmt"
+	//"fmt"
+	"encoding/json"
+	"github.com/google/go-cmp/cmp"
 	"strings"
 	"testing"
 )
 
-func TestLogger(t *testing.T) {
+func TestKeyValueWriter(t *testing.T) {
 	var tests = []struct {
 		in  []interface{}
 		out string
@@ -29,15 +31,16 @@ func TestLogger(t *testing.T) {
 	for _, tt := range tests {
 		var buf bytes.Buffer
 		logger := New(&Options{
-			Name:   "test",
-			Output: &buf,
+			Name:      "test",
+			Output:    &buf,
+			Formatter: NewKeyValueWriter(),
 		})
 
 		logger.Info(tt.in...)
 
 		str := buf.String()
 
-		fmt.Printf("output => %s\n", str)
+		//fmt.Printf("output => %s\n", str)
 
 		// Chop timestamp
 		dataIdx := strings.IndexByte(str, ' ')
@@ -47,4 +50,75 @@ func TestLogger(t *testing.T) {
 			t.Errorf("Info(%q) => %q, expected %q\n", tt.in, rest, tt.out)
 		}
 	}
+}
+
+func TestJSONWriter(t *testing.T) {
+	var tests = []struct {
+		in  []interface{}
+		out map[string]interface{}
+	}{
+		{
+			in:  []interface{}{"test message"},
+			out: map[string]interface{}{"@level": "info", "@name": "test", "message": "test message"},
+		},
+		{
+			in:  []interface{}{"test message", "name", "me"},
+			out: map[string]interface{}{"@level": "info", "@name": "test", "message": "test message", "name": "me"},
+		},
+		{
+			in:  []interface{}{"test message", "name", "me", "number", 2},
+			out: map[string]interface{}{"@level": "info", "@name": "test", "message": "test message", "name": "me", "number": float64(2)},
+		},
+	}
+
+	for _, tt := range tests {
+		var buf bytes.Buffer
+		logger := New(&Options{
+			Name:      "test",
+			Output:    &buf,
+			Formatter: NewJSONWriter(),
+		})
+
+		logger.Info(tt.in...)
+
+		b := buf.Bytes()
+
+		var raw map[string]interface{}
+		if err := json.Unmarshal(b, &raw); err != nil {
+			t.Fatal(err)
+		}
+
+		delete(raw, "@time")
+
+		if !cmp.Equal(raw, tt.out) {
+			t.Errorf("Info(%q) => %q, expected %q\n", tt.in, raw, tt.out)
+		}
+	}
+}
+
+// Comparison with hclog
+func BenchmarkLogger(b *testing.B) {
+	b.Run("info with 10 pairs", func(b *testing.B) {
+		var buf bytes.Buffer
+
+		logger := New(&Options{
+			Name:   "test",
+			Output: &buf,
+		})
+
+		for i := 0; i < b.N; i++ {
+			logger.Info("this is some message",
+				"name", "foo",
+				"what", "benchmarking yourself",
+				"why", "to see what's slow",
+				"k4", "value",
+				"k5", "value",
+				"k6", "value",
+				"k7", "value",
+				"k8", "value",
+				"k9", "value",
+				"k10", "value",
+			)
+		}
+	})
 }
